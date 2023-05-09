f253b7c25f51140404575f8f23f150d3322d097d
OFuturePica <116108818+OFuturePica@users.noreply.github.com>
2023-05-09 16:54:38 -0300

    final

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
+import { TesteService } from './teste.service';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'angular-pj1';
+  nomes: String[];
+
+  constructor(testService: TesteService) {
+    this.nomes = testService.getNomes();
+  }
 }
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,35 @@
+import { TesteService } from './teste.service';
+import { CoreModule } from './core/core.module';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NovoComponenteComponent } from './paginas/novo-componente/novo-componente.component';
+import { DiretivasComponent } from './diretivas/diretivas.component';
+import { DataBindingComponent } from './data-binding/data-binding.component';
+import { FormsModule } from '@angular/forms';
+import { HomeComponent } from './home/home.component';
+import { ProdutosComponent } from './produtos/produtos.component';
+import { PedidosComponent } from './pedidos/pedidos.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NovoComponenteComponent,
+    DiretivasComponent,
+    DataBindingComponent,
+    HomeComponent,
+    ProdutosComponent,
+    PedidosComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    CoreModule,
+    FormsModule
   ],
-  providers: [],
+  providers: [TesteService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
